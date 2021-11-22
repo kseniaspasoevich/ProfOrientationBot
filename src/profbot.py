5d6b2534518d227d52d8e7bb9aaa294e5a6ea0e1
kseniaspasoevich <kseniya.spasoyevich@gmail.com>
2021-11-22 13:11:35 +0300

    solve wemake conflicts 4

--- src/profbot.py
+++ src/profbot.py
@@ -4,6 +4,7 @@ from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import BotCommand
+
 from src.choice1 import register_handlers_basic_commands
 from src.choice2 import register_handlers_command_two
 from src.config import cfs
